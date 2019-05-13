b2408a6f8fa2b48f05ea5edec52c735b774acb39
Spanfile <spansdev@gmail.com>
2019-05-13 11:50:30 +0300

    Begin query stuff

--- common/src/schema/query.rs
+++ common/src/schema/query.rs
@@ -1,7 +1,27 @@
 use serde::{Deserialize, Serialize};
+use std::fs;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub enum Query {
     #[serde(rename = "ls")]
     Ls(String),
+    #[serde(rename = "cat")]
+    Cat(String),
+}
+
+impl Query {
+    pub fn run(&self) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+        match self {
+            Query::Ls(path) => {
+                let mut dirs = Vec::new();
+                for entry in fs::read_dir(path)? {
+                    let path = entry?.path();
+                    let name = path.file_name();
+                    dirs.push(String::from(name.unwrap().to_str().unwrap()));
+                }
+                Ok(dirs)
+            }
+            Query::Cat(path) => {}
+        }
+    }
 }
