15e4ff1e03dbd68567c9fd7c9d9c5005d953272a
Spanfile <spansdev@gmail.com>
2019-10-07 21:31:52 +0300

    Shell input stuff

--- src/shell/mod.rs
+++ src/shell/mod.rs
@@ -2,11 +2,21 @@ mod commands;
 mod completions;
 
 pub use commands::{Command, ExecutableCommand};
+use std::{
+    io::{self, Stdout, Write},
+    thread, time,
+};
+use termion::{
+    self, clear, cursor,
+    event::Key,
+    input::TermRead,
+    raw::{IntoRawMode, RawTerminal},
+};
 
-#[derive(Debug)]
 pub struct Shell {
     pub running: bool,
     mode: ShellMode,
+    stdout: RawTerminal<Stdout>,
 }
 
 #[derive(Debug)]
@@ -20,10 +30,73 @@ impl Shell {
         Shell {
             running: true,
             mode: ShellMode::Operational,
+            stdout: io::stdout()
+                .into_raw_mode()
+                .expect("couldn't set stdout into raw mode"),
         }
     }
 
     pub fn process_input(&mut self) -> Command {
+        let mut stdin = termion::async_stdin().keys();
+        let mut buffer = Vec::new();
+        let mut cursor_location: usize = 0;
+        let mut reading = true;
+
+        while reading {
+            if let Some(Ok(key)) = stdin.next() {
+                match key {
+                    Key::Left => {
+                        if cursor_location > 0 {
+                            write!(self.stdout, "{}", cursor::Left(1));
+                            cursor_location -= 1;
+                        }
+                    }
+                    Key::Right => {
+                        if cursor_location < buffer.len() {
+                            write!(self.stdout, "{}", cursor::Right(1));
+                            cursor_location += 1;
+                        }
+                    }
+                    Key::Backspace => {
+                        if cursor_location == 0 {
+                            break;
+                        }
+
+                        buffer.remove(cursor_location - 1);
+                        write!(self.stdout, "{}", cursor::Left(1));
+                        cursor_location -= 1;
+                    }
+                    Key::Char(c) => {
+                        write!(self.stdout, "{}", c);
+                        if c == '\n' {
+                            reading = false;
+                        } else {
+                            if cursor_location == buffer.len() {
+                                buffer.push(c);
+                            } else {
+                                buffer.insert(cursor_location, c);
+
+                                for b in buffer[cursor_location + 1..].iter() {
+                                    write!(self.stdout, "{}", b);
+                                }
+
+                                write!(
+                                    self.stdout,
+                                    "{}",
+                                    cursor::Left((buffer.len() - cursor_location - 1) as u16)
+                                );
+                            }
+                            cursor_location += 1;
+                        }
+                    }
+                    _ => continue,
+                }
+
+                self.stdout.lock().flush().expect("couldn't flush stdout");
+            }
+            thread::sleep(time::Duration::from_millis(50));
+        }
+
         unimplemented!();
     }
 
