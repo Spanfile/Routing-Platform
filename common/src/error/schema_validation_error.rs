2dc49700eed2823bf5ebc75091bcd01236a498fa
Spanfile <spansdev@gmail.com>
2019-10-12 15:09:43 +0300

    Implement display() for SchemaValidationError

--- common/src/error/schema_validation_error.rs
+++ common/src/error/schema_validation_error.rs
@@ -16,7 +16,32 @@ pub enum SchemaValidationError {
 
 impl CommonErrorTrait for SchemaValidationError {
     fn display(&self) -> String {
-        String::from("") // TODO
+        match self {
+            SchemaValidationError::Range { lower, upper } => format!(
+                "lower {:?} bound higher than upper {:?} bound",
+                lower, upper
+            ),
+            SchemaValidationError::DuplicateNodeName { name } => {
+                format!("duplicate node name: {}", name)
+            }
+            SchemaValidationError::Regex { regex, description } => {
+                format!("invalid regex {}: {}", regex, description)
+            }
+            SchemaValidationError::NoValues => String::from("no values defined"),
+            SchemaValidationError::MissingTemplate { template } => {
+                format!("template {} not found", template)
+            }
+            SchemaValidationError::NoMultipleValuesAllowed => {
+                String::from("multiple values defined where no multiple values are allowed")
+            }
+            SchemaValidationError::InvalidDefaultValue { default } => {
+                format!("default value {:?} doesn't match any given value", default)
+            }
+            SchemaValidationError::DuplicateProperty { property } => {
+                format!("duplicate property name: {}", property)
+            }
+            _ => String::from("other schema validation error"),
+        }
     }
 
     fn source(&self) -> Option<&(dyn error::Error + 'static)> {
