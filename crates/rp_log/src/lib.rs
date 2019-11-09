04ab085c1d85c0bdc602bf24ab89c477d8f83c48
Spanfile <spansdev@gmail.com>
2019-11-09 08:32:36 +0200

    Use colored outpout in logging

--- crates/rp_log/src/lib.rs
+++ crates/rp_log/src/lib.rs
@@ -1,17 +1,27 @@
+use fern::{
+    colors::{Color, ColoredLevelConfig},
+    Dispatch,
+};
 pub use log::{debug, error, info, trace, warn};
 
 pub fn setup_logging() -> anyhow::Result<()> {
-    fern::Dispatch::new()
-        .format(|out, msg, record| {
+    let colors = ColoredLevelConfig::new()
+        .info(Color::Green)
+        .debug(Color::Magenta)
+        .warn(Color::Yellow)
+        .error(Color::Red);
+    let time_format = "%Y-%m-%d %H:%M:%S";
+
+    Dispatch::new()
+        .format(move |out, msg, record| {
             out.finish(format_args!(
                 "[{} UTC] [{}] {}",
-                chrono::Utc::now().format("%Y-%m-%d %H:%M:%S"),
-                record.level(),
+                chrono::Utc::now().format(time_format),
+                colors.color(record.level()),
                 msg
             ))
         })
         .level(log::LevelFilter::Debug)
-        .chain(std::io::stdout())
         .apply()?;
     Ok(())
 }
