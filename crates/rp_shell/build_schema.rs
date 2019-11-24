2e0c3ee5a1dfc0a2bd3f5c712b2465596f7a2b04
Spanfile <spansdev@gmail.com>
2019-11-24 11:07:39 +0200

    Build and include debug schema

--- crates/rp_shell/build_schema.rs
+++ crates/rp_shell/build_schema.rs
@@ -6,15 +6,24 @@ fn main() -> anyhow::Result<()> {
     let pwd = env::current_dir()?;
 
     let schema_path = Path::new(&pwd).join("schema.yml");
+    let debug_schema_path = Path::new(&pwd).join("debug_schema.yml");
+
     let dest_path = Path::new(&out_dir).join("schema");
+    let debug_dest_path = Path::new(&out_dir).join("debug_schema");
 
     let schema_file = File::open(&schema_path)?;
     let mut schema = Schema::from_yaml_file(&schema_file)?;
     schema.validate()?;
     schema.build_regex_cache()?;
 
+    let debug_schema_file = File::open(&debug_schema_path)?;
+    let mut debug_schema = Schema::from_yaml_file(&debug_schema_file)?;
+    debug_schema.validate()?;
+
     let mut schema_dest = File::create(dest_path)?;
+    let mut debug_schema_dest = File::create(debug_dest_path)?;
     schema.to_binary_file(&mut schema_dest)?;
+    debug_schema.to_binary_file(&mut debug_schema_dest)?;
 
     Ok(())
 }
